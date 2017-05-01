10a1b46ffbe52d817e353b2b0ea5cdc41409d7d5
whatslve <whatslve@gmail.com>
2017-05-02 01:14:40 +0300

    йоу скоро будет пызда

--- app/public/game.js
+++ app/public/game.js
@@ -1,25 +1,41 @@
 
-var language, links, contentEl, navEl, state, day, stage;
-language = 'ru';
-contentEl = document.querySelector('.dialog');
-navEl = document.querySelector('.meny');
-links = {
-  startGame: "Йоу"
-};
-state = "meny";
+var language, links, contentEl, navEl, state, day, stage, karm, settings, authors;
+language = 'ru';//Язык диалогов
+contentEl = document.querySelector('.dialog');//обработчик диалога
+navEl = document.querySelector('.meny');//обработчик меню
+play = document.querySelector('.startGame');//обработчик старта игры
+settings = document.querySelector('.settings');//обработчик найстройки игры
+authors = document.querySelector('.authors');//обработчик авторы игры
+state = "meny";//Статус игры
 var updatestate = function(state){
 
-};
+};//обновить статус
 
 var saveGame = function(){
   setCookie("state", state, null);
-};
+};//сохранить текущий статус в куки
 
 var startGame = function(){
 navEl.setAttribute("class", "meny hidden");
 contentEl.setAttribute("class", "dialog show");
 
+};//начало игры
+var startSettings = function() {
+
 };
-navEl.addEventListener('click', function(){
+
+var startAuthors = function(){
+
+};
+
+play.addEventListener('click', function(){
   startGame();
-});
+});//клик по ссылке начать игру
+
+settings.addEventListener('click', function(){
+  startSettings();
+});//клик по ссылке настройки
+
+authors.addEventListener('click', function(){
+  startAuthors();
+});//клик по ссылке Авторы
