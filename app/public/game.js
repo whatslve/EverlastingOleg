1ded2efc3c355bd76abf78ecdbee4ecd636159c7
whatslve <whatslve@gmail.com>
2017-05-03 01:39:56 +0300

    Кароче тут пиздец я тут охуеваю но теперь чото работает

--- app/public/game.js
+++ app/public/game.js
@@ -1,33 +1,36 @@
 
-var language, links, contentEl, navEl, state, day, stage, karm, settings, authors;
+var language, links, contentEl, navEl, state, day, stage, karm, settings, authors,backMeny;
 language = 'ru';//Язык диалогов
 contentEl = document.querySelector('.dialog');//обработчик диалога
 navEl = document.querySelector('.meny');//обработчик меню
 play = document.querySelector('.startGame');//обработчик старта игры
 settings = document.querySelector('.settings');//обработчик найстройки игры
 authors = document.querySelector('.authors');//обработчик авторы игры
-state = "meny";//Статус игры
-var updatestate = function(state){
-
-};//обновить статус
-
+backMeny = document.querySelectorAll('.back-meny');//обработчик кнопки назад в меню
+mainMeny = document.querySelector('.window-main-meny');
+state="meny";
 var saveGame = function(){
   setCookie("state", state, null);
 };//сохранить текущий статус в куки
+var startMeny = function(){
 
+}
 var startGame = function(){
-navEl.setAttribute("class", "meny hidden");
-contentEl.setAttribute("class", "dialog show");
-alert("игра типо началось но еще нихуя нету");
+  navEl.setAttribute("class", "meny hidden");
+  contentEl.setAttribute("class", "dialog show");
+  alert("игра типо началось но еще нихуя нету");
 };//начало игры
 var startSettings = function() {
-alert("настройки ябать");
+  state="settings";
+  document.querySelector(".window-"+state).setAttribute("class", "window-"+state+ " show");
+  mainMeny.setAttribute("class", "window-main-meny hidden");
 };
 
 var startAuthors = function(){
-alert("авторы - whatslve & zurg3");
+  state="authors";
+  document.querySelector(".window-"+state).setAttribute("class", "window-"+state+ " show");
+  mainMeny.setAttribute("class", "window-main-meny hidden");
 };
-
 play.addEventListener('click', function(){
   startGame();
 });//клик по ссылке начать игру
@@ -39,3 +42,14 @@ settings.addEventListener('click', function(){
 authors.addEventListener('click', function(){
   startAuthors();
 });//клик по ссылке Авторы
+
+var backMenyClick = function(){
+  var object;
+  mainMeny.setAttribute("class", "window-main-meny show");
+  document.querySelector(".window-"+state).setAttribute("class", "window-"+state.toLowerCase()+" hidden");
+};
+for (i = 0; i < backMeny.length; ++i) {
+  backMeny[i].addEventListener('click',function(){
+    backMenyClick();
+  });
+};
